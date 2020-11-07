c6f40a42df05f12bd710f7bca6acde80293eb824
Ruslan Gainutdinov <ruslan@sanfrancisco.fi>
2020-11-07 23:26:54 +0200

    Fixed bug exposing internals

--- handler.js
+++ handler.js
@@ -5,8 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'Go Serverless v1.0! Your function executed successfully!'
       },
       null,
       2
